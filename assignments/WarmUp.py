16c1b6bacda9eed19b516a9dbb73e6cf4cbd6bba
Hallison Oliveira da Paz <hallpaaz@gmail.com>
2015-08-11 17:06:27 -0300

    working on function to automatize the tests

--- assignments/WarmUp.py
+++ assignments/WarmUp.py
@@ -1,23 +1,39 @@
-
+# Assignment 0. Python 3.4
 
 def mdc(a: int, b: int) -> int:
     """Returns the MDC between two natural numbers
 
     This function is an implementation of the Euclid's algorithm to compute the greatest common divisor (GDC) between two natural numbers. This algorithm does not use modular arithmetic so it won't work with negative integers. The name stands for the portuguese version."""
 
-    while (a - b) != 0:
-        if(a > b):
-            a, b = b, a-b
-        else:
-            b = b-a
-
-    return a
+    steps = 0
+    while b != 0:
+        a, b = b, a%b
+        steps += 1
+    return a, steps
 
 #script for execution
 if __name__ == "__main__":
-    while True:
-        a = int( input() )
-        b = int( input() )
-        if a == '' or b == '':
-            break
-        print( mdc(a, b) )
+
+    n = input("Digite o valor de N: ")
+    for limit in range(n):
+        run_mdc_with_limits(limit)
+
+
+
+def run_mdc_with_limits(limit: int, samples = 100000 ):
+    max_steps = 0
+    min_steps = -1
+    avg_steps = 0
+    for i in range(samples):
+        a = randrange(1, limit+1)
+        b = randrange(1, limit+1)
+        res, steps = mdc(a, b)
+
+        max_steps = steps > max_steps ? steps : max_steps
+        if steps < min_steps or min_steps == -1:
+            min_steps = steps
+        avg_steps += steps
+    avg_steps /= samples
+    with open("steps_data.csv", 'w+') as steps_file:
+        line = "{0}, {1}, {2}, {3}".format(limit, min_steps, max_steps, avg_steps)
+        steps_file.write(line)
