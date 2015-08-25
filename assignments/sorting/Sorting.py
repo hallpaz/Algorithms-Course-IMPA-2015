8d2a1d726ad07102f0e16f26e09b267bb28ea07d
Hallison da Paz <hallpaaz@gmail.com>
2015-08-24 22:39:58 -0300

    starting sorting assignment

---
+++ assignments/sorting/Sorting.py
@@ -0,0 +1,24 @@
+
+
+def selection_sort(array: list)->list:
+    pass
+
+def insertion_sort(array: list)->list:
+
+    for i in range(1, len(array)):
+        pass
+
+    pass
+
+def mergesort(array: list)->list:
+    pass
+
+def quicksort(array: list)->list:
+    pass
+
+def heapsort(array: list)->list:
+    pass
+
+
+if __name__ == "__main__":
+    print("Sorting file called as main script")
